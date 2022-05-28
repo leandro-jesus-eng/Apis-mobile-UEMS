cb3ec6825df94b08563d402e709997b308f0e970
Fernando Ayach <fernando@jeraapps.com>
2022-05-28 09:42:13 -0400

    [UPDATE] Finish ShareLote

--- app/src/main/java/com/apis/features/shareLote/ShareLoteActivity.java
+++ app/src/main/java/com/apis/features/shareLote/ShareLoteActivity.java
@@ -3,6 +3,7 @@ package com.apis.features.shareLote;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -15,12 +16,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.apis.R;
 import com.apis.data.database.relations.UserLoteCrossRef;
 import com.apis.data.repositories.DbRepository;
+import com.apis.data.repositories.EntitiesHandlerRepository;
 import com.apis.features.comportamentos_list.AdicionarComportamento;
 import com.apis.features.lote_list.LoteAdapter;
 import com.apis.features.lote_list.MainActivity;
@@ -33,15 +36,18 @@ import java.util.List;
 
 public class ShareLoteActivity extends AppCompatActivity {
 
-    EditText searchEditText;
-    ImageButton searchButton;
-    Button saveButton;
-    DbRepository dbRepository;
-    String loteName;
-    Integer loteId;
-    ArrayList<User> users = new ArrayList<>();
-    User currentUser;
-    ShareLoteAdapter adapter;
+    private EditText searchEditText;
+    private Button searchButton;
+    private Button saveButton;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private DbRepository dbRepository;
+    private EntitiesHandlerRepository entitiesHandlerRepository;
+    private String loteName;
+    private Integer loteId;
+    private ArrayList<User> users = new ArrayList<>();
+    private User currentUser;
+    private ShareLoteAdapter adapter;
+    private LinearLayout alertUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,17 +56,31 @@ public class ShareLoteActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        swipeRefreshLayout = findViewById(R.id.share_lote_swipeRefresh);
         searchEditText = findViewById(R.id.share_lote_search_editText);
         searchButton = findViewById(R.id.share_lote_search_button);
         saveButton = findViewById(R.id.share_lote_save_button);
+        alertUser = findViewById(R.id.alert_no_users_linearLayout);
         dbRepository = new DbRepository(this);
+        entitiesHandlerRepository = new EntitiesHandlerRepository(this);
         users.addAll(dbRepository.getAllUsers());
 
         getDataFromMain();
-        removeCurrentUser();
+        removeUnnecessaryUsers();
+        setupSwipeRefresh();
         setListeners();
         setupList(users);
         getSupportActionBar().setTitle("Compartilhar "+loteName+" com outros usuários");
+    }
+
+    private void setupSwipeRefresh() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                setupList(users);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 
     private void getDataFromMain() {
@@ -71,12 +91,19 @@ public class ShareLoteActivity extends AppCompatActivity {
         }
     }
 
-    private void removeCurrentUser() {
-        for(User user : users) {
-            if (user.getEmail().equals(currentUser.getEmail())) {
-                users.remove(user);
-                break;
+    private void removeUnnecessaryUsers() {
+        List<User> nonNecessaryUsers = new ArrayList<>();
+        for (User user : users) {
+            if (user.getEmail().equals(currentUser.getEmail()) || entitiesHandlerRepository
+                    .userLoteCrosRefExists(user.getUserId(), loteId)
+            ) {
+                nonNecessaryUsers.add(user);
             }
+        }
+
+        for (User nonNecessaryUser : nonNecessaryUsers) {
+            users.contains(nonNecessaryUser);
+            users.remove(nonNecessaryUser);
         }
     }
 
@@ -99,9 +126,19 @@ public class ShareLoteActivity extends AppCompatActivity {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.share_lote_user_recycleView);
         List<ItemUser> itemUsers = new ArrayList<>();
         for (User user : users) { itemUsers.add(new ItemUser(false, user)); }
-
+        showNoUsersAlert(itemUsers);
         adapter = new ShareLoteAdapter(itemUsers, this);
         recyclerView.setAdapter(adapter);
+    }
+
+    private void showNoUsersAlert(List<ItemUser> users) {
+        if(users.size() > 0)  {
+            alertUser.setVisibility(View.INVISIBLE);
+            saveButton.setVisibility(View.VISIBLE);
+        } else {
+            alertUser.setVisibility(View.VISIBLE);
+            saveButton.setVisibility(View.INVISIBLE);
+        }
     }
 
     private void onSearch() {
@@ -134,14 +171,11 @@ public class ShareLoteActivity extends AppCompatActivity {
                     .setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
-                                    for(Integer userId : selectedUsersId) {
+                                    for (Integer userId : selectedUsersId) {
                                         Log.i("UZER", userId.toString());
-                                    /*
-                                    dbRepository.insertUserLoteCrossRef(
-                                            new UserLoteCrossRef(userId, loteId)
-                                    );
-
-                                     */
+                                        dbRepository.insertUserLoteCrossRef(
+                                                new UserLoteCrossRef(userId, loteId)
+                                        );
                                     }
                                     Toast.makeText(
                                             getApplicationContext(),
